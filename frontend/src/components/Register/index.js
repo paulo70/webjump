80c04e22efab9f37e2f6b2ec0fd98891e87351e4
paulo70 <paulofrontend@gmail.com>
2020-07-10 12:06:33 -0300

    structure html to register component

---
+++ frontend/src/components/Register/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import './register.scss'
+
+function Register (){
+  return (
+    <div className='container-fluid register'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-xs-12'>
+            <ul className='register-login'>
+              <li>Acesse sua conta</li>
+              <li>ou</li>
+              <li>Cadastre-se</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Register
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import App from './app'
+import Register from './components/Register'
 
 ReactDOM.render(
   <>
+    <Register />
     <App />
   </>,
   document.getElementById('root'))
