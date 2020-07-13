fdc6974af06beb22ac6671ab414187f1eb8e45c6
paulo70 <paulofrontend@gmail.com>
2020-07-12 23:03:36 -0300

    adjust filter and remove lig bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import App from './app'
